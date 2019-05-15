c367b0390ff07467af5b3c9a7326e0e55d879245
Gregory <gerda.trust@gmail.com>
2019-05-15 12:30:29 +0200

    Add list of article, sort and  search panel
    in a search method use trim() to cancel spaces in search phrase

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news')
 
 /* GET home page. */
 router.get('/', (req, res) => {
-    res.render('news', { title: 'News' });
+    // console.log(req.query);
+    const search = req.query.search || ''; // || ''  dodane zeby zabezpieczyc przed pustym stringiem w search i zeby dodany do metody search trim() (serach.trim() - nie wyrzucał błędu)
+
+    const findNews = News
+        // .find({ title: search })
+        .find({ title: new RegExp(search.trim(), 'i') }) // i- insentitive - nie czuły na wielkośc liter ?
+        .sort({ created: -1 })  // sort words out in alphabetical order from A to Z
+
+    findNews.exec((err, data) => {
+        // console.log(data);
+        res.render('news', { title: 'News', data, search });
+    })
+
 });
 
 
